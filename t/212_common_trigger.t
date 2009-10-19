a874bd19d1f0fc67adebd7b6c92ee5f4c6914b14
Ryo Miyake <ryo.studiom@gmail.com>
2009-10-19 15:39:06 +0900

    added trigger tests

--- t/212_common_trigger.t
+++ t/212_common_trigger.t
@@ -10,11 +10,27 @@ describe 'common trigger test' => run {
     };
 
     test 'common trigger' => run {
-        my $row = Mock::CommonTrigger->insert('mock_triggered',{
+        my $row = Mock::CommonTrigger->insert('mock_common_trigger',{
             id   => 1,
         });
         isa_ok $row, 'DBIx::Skinny::Row';
         is $row->created_at, 'now_s';
+    };
+
+    test 'common and table own trigger' => run {
+        my $row = Mock::CommonTrigger->insert('mock_both_triggers',{
+            id   => 1,
+        });
+        isa_ok $row, 'DBIx::Skinny::Row';
+        is $row->created_at, 'now_s(custom)';
+    };
+
+    test 'trigger operates not exists column' => run {
+        my $row = Mock::CommonTrigger->insert('mock_lack_column',{
+            id   => 1,
+        });
+        isa_ok $row, 'DBIx::Skinny::Row';
+        is_deeply [qw/id updated_at/], [sort keys %{$row->{row_data}}];
     };
 };
 
--- t/Mock/CommonTrigger.pm
+++ t/Mock/CommonTrigger.pm
@@ -8,9 +8,22 @@ use DBIx::Skinny setup => +{
 sub setup_test_db {
     my $db = shift;
     $db->do(q{
-        CREATE TABLE mock_triggered (
+        CREATE TABLE mock_common_trigger (
             id         INT,
             created_at TEXT,
+            updated_at TEXT
+        )
+    });
+    $db->do(q{
+        CREATE TABLE mock_both_triggers (
+            id         INT,
+            created_at TEXT,
+            updated_at TEXT
+        )
+    });
+    $db->do(q{
+        CREATE TABLE mock_lack_column (
+            id         INT,
             updated_at TEXT
         )
     });
@@ -19,19 +32,39 @@ sub setup_test_db {
 package Mock::CommonTrigger::Schema;
 use DBIx::Skinny::Schema;
 
-install_table mock_triggered => schema {
+install_table mock_common_trigger => schema {
     pk 'id';
     columns qw/id created_at updated_at/;
 };
 
-install_common_trigger pre_insert => sub {
-    my ($self, $args) = @_;
-    $args->{created_at} ||= 'now';
+install_table mock_both_triggers => schema {
+    pk 'id';
+    columns qw/id created_at updated_at/;
+    trigger pre_insert => callback {
+        my ($self, $args, $table) = @_;
+        my $columns = $self->schema->schema_info->{$table}->{columns};
+        $args->{ created_at } .= '(custom)'
+            if grep {/^created_at$/} @$columns;
+    };
+};
+
+install_table mock_lack_column => schema {
+    pk 'id';
+    columns qw/id updated_at/;
 };
 
 install_common_trigger pre_insert => sub {
-    my ($self, $args) = @_;
-    $args->{created_at} .= '_s';
+    my ($self, $args, $table) = @_;
+    my $columns = $self->schema->schema_info->{$table}->{columns};
+    $args->{created_at} ||= 'now'
+        if grep {/^created_at$/} @$columns;
+};
+
+install_common_trigger pre_insert => sub {
+    my ($self, $args, $table) = @_;
+    my $columns = $self->schema->schema_info->{$table}->{columns};
+    $args->{created_at} .= '_s'
+        if grep {/^created_at$/} @$columns;
 };
 
 1;
