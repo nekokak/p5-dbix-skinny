ea63ff6f1de8ae3cec2cb2cbb886216d8333b134
Atsushi Kobayashi <nekokak@gmail.com>
2010-03-07 22:14:01 +0900

    _executeでprofileとってるからここではいらない（thanks makamaka-san

--- lib/DBIx/Skinny/DBD/mysql.pm
+++ lib/DBIx/Skinny/DBD/mysql.pm
@@ -41,7 +41,6 @@ sub bulk_insert {
     my $values = '(' . join(', ', ('?') x @cols) . ')' . "\n";
     $sql .= join(',', ($values) x (scalar(@bind) / scalar(@cols)));
 
-    $skinny->profiler($sql, \@bind);
     $skinny->_execute($sql, \@bind);
 
     return 1;
