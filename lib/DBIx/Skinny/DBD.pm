5db929551c68a624c6a3fe6ef21db2b8dd10fbed
Atsushi Kobayashi <nekokak@gmail.com>
2010-11-04 16:59:33 +0900

    confessしないとわかりにくすぎる

--- lib/DBIx/Skinny/DBD.pm
+++ lib/DBIx/Skinny/DBD.pm
@@ -1,14 +1,15 @@
 package DBIx::Skinny::DBD;
 use strict;
 use warnings;
+use Carp ();
 
 sub new {
     my ($class, $dbd_type) =@_;
-    die "No Driver" unless $dbd_type;
+    Carp::confess "No Driver" unless $dbd_type;
 
     my $subclass = join '::', $class, $dbd_type;
     eval "use $subclass"; ## no critic
-    die $@ if $@;
+    Carp::confess $@ if $@;
     bless {}, $subclass;
 }
 
